e8fc9d965e1aa3590effcfbcee6854257a8ca7bc
Lígia Bicalho <ligiabicalho@gmail.com>
2022-10-19 16:57:40 -0300

    Req 2. Teste o componente <About />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('2 - Teste o componente <About />', () => {
+  it('Verifica se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutTitle = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('Verifica se a página contém uma imagem da Pokédex', () => {
+    renderWithRouter(<About />);
+
+    // const img = screen.getByAltText('Pokédex'); //
+    // expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+
+  // it('Verifica se ', () => {
+  //   renderWithRouter(<About />);
+  //   const  = screen.getByRole();
+  //   expect().toBeInTheDocument();
+  // });
+});
