2eb25670d976b5b44cab69dfe4a8bf52e2d4925a
Lígia Bicalho <ligiabicalho@gmail.com>
2022-10-19 17:47:53 -0300

    Req 3. Teste o componente <FavoritePokemons />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import About from '../pages/About';
 import renderWithRouter from '../renderWithRouter';
 
@@ -14,16 +13,10 @@ describe('2 - Teste o componente <About />', () => {
   it('Verifica se a página contém uma imagem da Pokédex', () => {
     renderWithRouter(<About />);
 
-    // const img = screen.getByAltText('Pokédex'); //
+    // const img = screen.getByAltText('Pokédex');
     // expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 
     const img = screen.getByRole('img');
     expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
-
-  // it('Verifica se ', () => {
-  //   renderWithRouter(<About />);
-  //   const  = screen.getByRole();
-  //   expect().toBeInTheDocument();
-  // });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+import pokemons from '../data';
+
+describe('3 - Teste o componente <FavoritePokemons />', () => {
+  it('Verifique se é exibida na tela a mensagem `No favorite pokemon found`, caso a pessoa não tenha pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+
+    const noFavoritePokemons = screen.getByText('No favorite pokemon found');
+    expect(noFavoritePokemons).toBeInTheDocument();
+  });
+
+  it('Verifique se são exibidos todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const favoritePokemons = screen.getAllByAltText(/is marked as favorite/i);
+    expect(favoritePokemons).toHaveLength(pokemons.length);
+  });
+});
