9a711928ac48d94c40c61b558fd8b3f2c629c01a
Lígia Bicalho <ligiabicalho@gmail.com>
2022-10-20 17:35:29 -0300

    Refatorando requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -16,15 +16,16 @@ beforeEach(() => {
 describe('7- Teste o componente <PokemonDetails />', () => {
   it('Verifique se as informações detalhadas do pokémon selecionado são mostradas na tela.', () => {
     // A página deve conter um texto < name > Details, onde < name > é o nome do pokémon;
-    const heading2 = screen.getAllByRole('heading', { level: 2 });
-    expect(heading2[0]).toHaveTextContent('Pikachu Details');
+    const textDetails = screen.getByText('Pikachu Details');
+    expect(textDetails).toBeInTheDocument();
 
     // Não deve existir o link de navegação para os detalhes do pokémon selecionado;
     const detailsLink = screen.queryByRole('link', { name: 'More details', href: '/pokemons/25' });
     expect(detailsLink).not.toBeInTheDocument();
 
     // A seção de detalhes deve conter um heading h2 com o texto Summary;
-    expect(heading2[1]).toHaveTextContent('Summary');
+    const headingSummary = screen.getByRole('heading', { name: 'Summary', level: 2 });
+    expect(headingSummary).toBeInTheDocument();
 
     // A seção de detalhes deve conter um parágrafo com o resumo do pokémon específico sendo visualizado.
     const summaryPoke = screen.getByText(/roasts hard berries with electricity/i);
@@ -32,8 +33,8 @@ describe('7- Teste o componente <PokemonDetails />', () => {
   });
   it('Verifique se existe na página uma seção com os mapas contendo as localizações do pokémon', () => {
     // Na seção de detalhes deverá existir um heading h2 com o texto Game Locations of < name > do pokémon exibido;
-    const heading2 = screen.getAllByRole('heading', { level: 2 });
-    expect(heading2[2]).toHaveTextContent('Game Locations of Pikachu');
+    const headingLocation = screen.getByRole('heading', { name: 'Game Locations of Pikachu', level: 2 });
+    expect(headingLocation).toBeInTheDocument();
 
     // Todas as localizações do pokémon devem ser mostradas na seção de detalhes;
     // Devem ser exibidos o nome da localização e uma imagem do mapa em cada localização;
